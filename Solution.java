46e42d1b3306e856e84f96c1da4c8f7fc91d90b4
Venkatesan <anuj.venkatesan@fiserv.com>
2023-11-01 15:10:52 -0400

    finished work

--- Solution.java
+++ Solution.java
@@ -1,12 +1,45 @@
-class Solution {
+import java.util.ArrayList;
+
+public class Solution {
     public static char mostFrequentChar(String s) {
+      String unique = "";
+      ArrayList<Integer> count = new ArrayList<Integer>();
+      for(int i =0; i < s.length();i++)
+      {
+        String c = s.substring(i,i+1);
+        
+        if(!unique.contains(c))
+        {
+          
+          
+          unique += c;
+          count.add(0);
+        }
+        count.set(unique.indexOf(c),count.get(unique.indexOf(c))+ 1);
+       
+        
+
+      }
+     
+      int max = 0;
+      char maxchar = 'a';
+      for(int j =0; j < count.size();j++)
+      {
+        
+        if(count.get(j)>max)
+        {
+          max = count.get(j);
+          maxchar = unique.charAt(j);
+        }
+      }
       
-      return;
+      return maxchar;
+      
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
     }
 
   }
